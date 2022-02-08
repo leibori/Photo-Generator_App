23235428cc24c0454b197525f5879db1b44323be
Ori Leibovitz <45977036+leibori@users.noreply.github.com>
2022-02-08 18:02:27 +0200

    Add files via upload

---
+++ RandomPic/RandomPicUITests/RandomPicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RandomPicUITestsLaunchTests.swift
+//  RandomPicUITests
+//
+//  Created by Ori Leibovitz on 08/02/2022.
+//
+
+import XCTest
+
+class RandomPicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
